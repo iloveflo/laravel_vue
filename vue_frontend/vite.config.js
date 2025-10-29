4b51ae983d169787a5d19902b006cbee60754fb6
iloveflo <binha10k56@gmail.com>
2025-10-29 09:19:27 +0700

    đã cấu hình proxy tích hợp 2 cổng ,không cần cấu hình CORS nữa

--- vue_frontend/vite.config.js
+++ vue_frontend/vite.config.js
@@ -1,7 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/frontend/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: 'dist', // Thư mục output khi build
+  },
+  
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // địa chỉ Laravel
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
 })
