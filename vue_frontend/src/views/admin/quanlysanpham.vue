f1262f39e9bbe35920979607ef6d91cd0ae244fb
iloveflo <binha10k56@gmail.com>
2025-11-20 00:07:30 +0700

    tao khung admin,index

---
+++ vue_frontend/src/views/admin/quanlysanpham.vue
@@ -0,0 +1,5 @@
+<template>
+<h2>Quản lý sản phẩm</h2>
+</template>
+<script setup>
+</script>
---
+++ vue_frontend/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+<Header />
+<div class="content">
+<h1>Trang chủ</h1>
+</div>
+<Footer />
+</template>
+
+
+<script setup>
+import Header from '../components/header.vue'
+import Footer from '../components/footer.vue'
+</script>
+
+
+<style scoped>
+.content {
+padding: 20px;
+}
+</style>
